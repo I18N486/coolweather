cad96662b7ae9f91e70a1a0b87ca5693f45dce61
zhangshitao <zhangshitao@example.com>
2017-08-01 09:23:52 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/zst/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.zst.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhangshitao on 8/1/17.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
